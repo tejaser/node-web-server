8ac651f1e0c91f9f111964e84ae073f2447e9549
tejaser <tejas.er@outlook.com>
2018-01-13 11:03:47 +0530

    added logging functionality

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const hbs = require("hbs");
+const fs = require("fs");
 
 var app = express();
 
+hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile("server.log", log + "\n");
+  next();
+});
+
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
